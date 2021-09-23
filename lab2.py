02cf0e659d1744263d52a6d94b24fee8ae727449
Katie Beattie <mkbeattie@loyola.edu>
2021-09-23 11:07:32 -0400

    i created the file.

--- lab2.py
+++ lab2.py
@@ -2,3 +2,13 @@
 # Course: CS151 Dr. Rajeev
 # September 23, 2021
 # Lab 2
+
+# creation of input variable
+birthInput = input("Enter the number of births per second of the population. ")
+# typecast to int
+birthInt = int(birthInput)
+# repetition of above processes twice
+deathInput = input("Enter the number of deaths per second of the population. ")
+deathInt = int(deathInput)
+popInput = input("Enter the population size. ")
+popInt = int(popInput)

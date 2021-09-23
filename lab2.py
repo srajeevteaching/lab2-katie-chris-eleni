04cdbb058217644bdafe55ea08ccc7c572ffbcca
Katie Beattie <mkbeattie@loyola.edu>
2021-09-23 11:34:09 -0400

    i created the file.

--- lab2.py
+++ lab2.py
@@ -10,19 +10,23 @@ births = float(births)
 # repetition of above processes twice
 deaths = input("Enter the number of deaths per second of the population. ")
 deaths = float(deaths)
-immigrations = input("Enter the number of immigrants. ")
+immigrations = input("Enter the number of immigrants leaving. ")
 pop = input("Enter the population size. ")
 pop = float(pop)
 Years = input("Enter the desired projected years")
 Years = int(Years)
 
-#computations for determining population at desired projected year
+# computations for determining population at desired projected year
 DAY = pop - (Years * deaths)
 DAY = float(DAY)
 IAY = pop - (Years * immigrations)
 IAY = float(IAY)
 BAY = pop + (Years * births)
 BAY = float(BAY)
+
+# births after so many years: births per second * 60 = minute * 60 = hour * 24 = day * 365 = year * years#
+# deaths after so many years: deaths per second * 60 = minute * 60 = hour *24 = day * 365 = year * years#
+# immigrants after so many years: imm. per second * 60 = minute * 60 = hour * 24 = day * 365 = year * years#
 
 
 
